9c475a23fe1b3e0d91ff45898a6463e4770b63e8
Leone Varando <106971202+Opaleone@users.noreply.github.com>
2022-10-04 23:14:30 -0500

    Finished leaderboard and readme files

--- README.md
+++ README.md
@@ -1,1 +1,73 @@
-# Challenge-4---Code-Test
+# Code Test | For Beginners
+
+## What is this?
+
+This is a code quiz that has been designed using DOM manipulation.
+
+### Website Link
+
+ https://opaleone.github.io/Code-Test-for-Beginners/
+
+>This is an image representing the landing page
+![landing-page](assets/Images/main-page.png)
+
+## How is it done
+
+Firstly, I had to break this down into 3 main parts. 
+
+    Timer
+    Question Display
+    Leaderboard
+
+## Timer
+---
+
+### Timer Image
+![image-of-timer](assets/Images/timer-view.png)
+
+I started with my timer. I created a timer that counts down from 60 seconds by decrementing a variable I created by 1 every 1000 miliseconds.
+
+Next, I created a function that starts the quiz whenever a button is pressed. This function also starts my timer which doubles as a score.
+
+## Question Display
+---
+
+### Question Image 
+![image-of-question](assets/Images/question-display.png)
+
+Here, I began my functions for displaying the question. I separated this into two different functions.
+
+The first function I wrote was how to display a question. This simply tells Javascript which questions to display and when to display them using IF statement and FOR loops. When there are no more questions left to display, determined by COUNT variable, the function will hide the question section, place the score in LOCAL STORAGE and run the function to display the leaderboard.
+
+The first function I wrote was determines when Javascript will proceed to the next question. It listens for a button press then, using an IF statement, determines, based of my correctAnswerIndex variable, if the button pressed hold the correct answer. If so, it adds 6 to the timer, which is score. If the answer is incorrect, it subtracts 12 from the timer.
+
+## Leaderboard
+---
+
+### Leaderboard Image
+![image-of-leaderboard](assets/Images/leaderboard-link.png)
+
+The last step of this quiz was the leaderboard. The intent of this section to grab the last score of the user and display this on the screen for them both after the complete the quiz, and whenever the click the View Highscore button on the top left of the webpage. This required two functions as well.
+
+The first function is to display the leaderboard after the quiz is finished.    
+    
+    This function is called in the ELSE of the IF statement within the displayQuestion function. 
+    
+It appends an h1 element and an p element within the leaderboard section of the HTML file and changes their text content to show the last score of the user.
+
+The next function does the same thing as the last function. However, there is one important line added to this function. It hides the landing page whenever it is clicked.
+
+## Credits
+
+The University of Texas - Austin
+
+[StackOverflow.com](https://stackoverflow.com/)
+
+[MDN Documentation](https://developer.mozilla.org/en-US/docs/Web/JavaScript)
+
+[W3-Schools](https://www.w3schools.com/js/)
+
+## License
+
+Please refer to license in the repository.
+
--- assets/script.js
+++ assets/script.js
@@ -1,19 +1,17 @@
+// Query selectors for DOM manipulation later
+
 var question = document.getElementById('question');
 var answer = document.querySelector('#question-answers');
 var intro = document.querySelector('#introduction');
-var answer1 = document.querySelector('#answer1');
-var answer2 = document.querySelector('#answer2');
-var answer3 = document.querySelector('#answer3');
-var answer4 = document.querySelector('#answer4');
 var timer = document.querySelector('#timer');
-var highscores = document.querySelector('#highscore');
+var highScores = document.querySelector('#highscore');
 var leaderBoard = document.querySelector('#leaderboard');
-
-var count = 0;
-
 const start = document.querySelector('#start-button');
 
+// Check for question number
+var count = 0;
 
+// Question and Answer objects for reference
 const questions = {
   question1: 'Which is NOT a primitive data type?',
   question2: 'What does CSS stand for?',
@@ -33,13 +31,15 @@ const answers = {
 
 const correctAnswerIndex = [3, 3, 3, 2, 3];
 
+// Timer variables
+
 var secondsLeft = 60;
 
 var timerInterval;
 
-var scoreHistory = localStorage.getItem('highScores');
+// Variable that stores values retrieved from computers local storage
 
-var scores = JSON.parse(scoreHistory);
+var scores = localStorage.getItem('highScore');
 
 // Timer
 
@@ -49,12 +49,10 @@ function timerCounter() {
   
   if (secondsLeft === 0) {
     clearInterval(timerInterval);
-    // All steps required to end game
   }
 }
 
-
-// Questions
+// Starts the timer and displays next question
 
 function startQuiz() {
 
@@ -65,17 +63,34 @@ function startQuiz() {
   displayQuestion();
 }
 
+// Question Display
+
+function displayQuestion() {
+
+  if (count < 5) {
+    question.children[0].textContent = questions[Object.keys(questions)[count]];
+    let myArr = answers[Object.keys(answers)[count]];
+
+    console.log(myArr);
+
+    for (let i = 0; i < 4; i++) {
+      answer.children[i].textContent = myArr[i];
+    }
+  } else {
+    question.style.display = 'none';
+    clearInterval(timerInterval);
+    timer.textContent = secondsLeft;
+    localStorage.setItem('highScore', secondsLeft);
+    var latestScore = localStorage.getItem('highScore')
+    displayLeaderboard(latestScore);
+  }
+}
+
 function nextQuestion(evt) {
-  // Set new questions and answers
-  // let wtf = question.childElementCount;
-
-  // console.log(question.children[wtf]);
-
 
   if (question.childElementCount === 3) {
     question.lastChild.remove();
   } 
-
 
   let userInput = evt.target.getAttribute('id');
   let pEl = document.createElement('p');
@@ -83,9 +98,6 @@ function nextQuestion(evt) {
   pEl.setAttribute('id', 'status');
 
   userInput = parseInt(userInput[userInput.length-1]) - 1;
-
-  // console.log(userInput);
-
 
   if (userInput === correctAnswerIndex[count]) {
     pEl.textContent = 'Correct!';
@@ -96,56 +108,59 @@ function nextQuestion(evt) {
     question.appendChild(pEl);
     secondsLeft -= 12;
   }
-  
-  
-  // console.log(`User Input ${userInput}`);
-  // console.log(`Correct Answer Array: ${correctAnswerIndex[count]}`);
 
   count++;
-  // console.log(`Count: ${count}`);
   displayQuestion();
 
 }
 
-function displayQuestion() {
 
-  if (count < 5) {
-    question.children[0].textContent = questions[Object.keys(questions)[count]];
-    let myArr = answers[Object.keys(answers)[count]];
-
-    for (let i = 0; i < 4; i++) {
-      answer.children[i].textContent = myArr[i];
-    }
-  } else {
-    question.style.display = 'none';
-    clearInterval(timerInterval);
-    timer.textContent = secondsLeft;
-    var leaderBoard = prompt(`Here is your score: ${secondsLeft}.\nPlease enter you name:`)
-    scores = Object.assign({leaderBoard: secondsLeft});
-    console.log(scores)
-    localStorage.setItem('highScores', JSON.stringify(scores));
-
-    displayLeaderboard();
-  }
-
-  // count++
-  // console.log(`Count: ${count}`);
-}
 
 
 // Leaderboard
 
-function displayLeaderboard() {
+function displayLeaderboard(num) {
+  var titleEl = document.createElement('h1');
+  var scoreDisplay = document.createElement('p');
+
+  titleEl.setAttribute('id', 'leaderboard-title');
+  scoreDisplay.setAttribute('id', 'score-display');
+
+  scoreDisplay.textContent = num;
+
+  titleEl.textContent = 'Your last score is:'
+
+  leaderBoard.appendChild(titleEl);
+  leaderBoard.appendChild(scoreDisplay);
+
+  leaderBoard.style.display = 'flex';
   
-
-
 }
 
-start.addEventListener('click', startQuiz);
+function leaderBoardLink() {
+  intro.style.display = 'none';
 
+  var titleEl = document.createElement('h1');
+  var scoreDisplay = document.createElement('p');
+
+  titleEl.setAttribute('id', 'leaderboard-title');
+  scoreDisplay.setAttribute('id', 'score-display');
+
+  scoreDisplay.textContent = scores;
+
+  titleEl.textContent = 'Your last score is:'
+
+  leaderBoard.appendChild(titleEl);
+  leaderBoard.appendChild(scoreDisplay);
+
+  leaderBoard.style.display = 'flex';
+}
+
+// Event listeners for Buttons
+
+start.addEventListener('click', startQuiz);
 answer1.addEventListener('click', nextQuestion);
 answer2.addEventListener('click', nextQuestion);
 answer3.addEventListener('click', nextQuestion);
 answer4.addEventListener('click', nextQuestion);
-
-// highscores.addEventListner('click', displayLeaderboard);
+highScores.addEventListener('click', leaderBoardLink);
